f68b65a4304ca0ee313492d141ff11cab2aa7ee0
asrarbutt <asrarbutt1986@gmail.com>
2022-07-13 15:49:31 +0200

    initial commit

---
+++ src/main/java/com/example/gitwithbranches/GitWithBranchesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitwithbranches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWithBranchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitWithBranchesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitwithbranches/GitWithBranchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitwithbranches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWithBranchesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
